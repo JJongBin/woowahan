7489418d0cf080409470ac405b41bb0e1646447d
원종빈의 MacBook <gghkdu2@gmail.com>
2021-07-22 17:31:46 +0900

    modal main-menu 클릭시에도 modal 닫히던 문제 해결

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const logo = document.querySelector("#header .logo");
-const bg = document.querySelector("#modal");
-const menu = bg.querySelector("#main-menu");
+const modal = document.querySelector("#modal");
+const bg = document.querySelector("#bg")
+const menu = document.querySelector("#main-menu");
 
 logo.addEventListener("mouseover", function() {
     logo.classList.remove("menu-change2")
@@ -13,19 +14,18 @@ logo.addEventListener("mouseout", function() {
 })
 
 logo.addEventListener("click", function () {
-    if(bg.classList.contains("hidden")){
-        bg.classList.remove("hidden");
+    if(modal.classList.contains("hidden")){
+        modal.classList.remove("hidden");
     }else{
-        bg.classList.add("hidden");
+        modal.classList.add("hidden");
     }
 
 })
 bg.addEventListener("click", function() {
-    bg.classList.add("hidden")
-    console.log("click")
+    modal.classList.add("hidden")
 })
 menu.addEventListener("click", function(){
     // console.log("click")
 });
 
-// bg를 클릭시에 사라지는데 bg안의 메뉴를 클릭해도 사라짐  
+
