9ef2c068b979322378714e7574353707519a80f4
원종빈의 MacBook <gghkdu2@gmail.com>
2021-07-22 17:27:52 +0900

    main menu 추가

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 const logo = document.querySelector("#header .logo");
-const bg = document.querySelector("#bg")
+const bg = document.querySelector("#modal");
+const menu = bg.querySelector("#main-menu");
 
 logo.addEventListener("mouseover", function() {
     logo.classList.remove("menu-change2")
@@ -17,9 +18,14 @@ logo.addEventListener("click", function () {
     }else{
         bg.classList.add("hidden");
     }
-    bg.addEventListener("click", function() {
-        bg.classList.add("hidden")
-    })
+
 })
+bg.addEventListener("click", function() {
+    bg.classList.add("hidden")
+    console.log("click")
+})
+menu.addEventListener("click", function(){
+    // console.log("click")
+});
 
-
+// bg를 클릭시에 사라지는데 bg안의 메뉴를 클릭해도 사라짐  
